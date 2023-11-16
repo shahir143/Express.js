c168d52a817ba3b64588d196de3852e81b7a09c8
shahir143 <Shahirsyed143@gmail.com>
2023-11-16 11:55:57 +0530

    first express

---
+++ task-1/index.js
@@ -0,0 +1,16 @@
+const http= require('http');
+const express= require('express');
+
+const app= express();
+
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("In the  another middleware");
+    res.send('<h1>Hello from Another middleware</h1>')
+});
+
+app.listen(4000);
